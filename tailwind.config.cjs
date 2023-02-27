821351e6e5de779453578f4c8a835884935253dd
Andres Laguilavo <lagui2003@gmail.com>
2023-02-27 17:53:15 -0500

    feat: add some custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    fontFamily:{
-      montserrat: ['Montserrat', 'sans-serif']
+    fontFamily: {
+      montserrat: ["Montserrat", "sans-serif"],
     },
 
     textColor: (theme) => ({
-      ...theme('colors'),
-      redBrand: "#CC2D4A"
+      ...theme("colors"),
+      primary: "#CC2D4A",
+    }),
+
+    backgroundColor: (theme) => ({
+      ...theme("colors"),
+      greenBrand: "#8FA206",
     }),
 
     extend: {},
+
   },
-  plugins: [],
 }
